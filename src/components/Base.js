30a50eef2acc1c1fd299f0fe5d40e9308703137c
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-20 19:20:53 -0400

    added files,completed base,home,nav also added styling

---
+++ src/components/Base.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from '../Nav';
+import Home from './home/Home';
+import About from './about/About';
+import Portfolio from './portfolio/Portfolio';
+import { Route, Routes } from 'react-router-dom';
+import { Box, Grid } from '@mui/material';
+
+export default function Base() {
+
+
+  return (
+    <Box>
+      <Grid container display="flex" flexDirection="column" minHeight="100vh" justifyContent="space-between">
+        <Grid item>
+          <Navbar/>
+        </Grid>
+        <Grid item flexGrow={1}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </Grid>
+        <Grid item>
+          <Box component="footer" display="flex" flexDirection="column" alignItems="center" py="1.5rem" sx={{ opacity: 0.7 }} width="100%">
+            <p>created by &hearts; Reagan Yang</p>
+            <p>&copy; 2023</p>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Style from './Navbar.module.scss';
+import { Link } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { info } from '../info/Info';
+
+const navLinks = [
+  { name: 'Home', to: '/', key: 'home' },
+  { name: 'About Me', to: '/about', key: 'about' },
+  { name: 'Portfolio', to: '/portfolio', key: 'portfolio' }
+];
+
+const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(null);
+
+  return (
+    <Box component="nav" width="100%">
+      <Box
+        component="ul"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        gap={{ xs: '2rem', md: '8rem' }}
+        textTransform="lowercase"
+        fontSize="1rem"
+      >
+        {navLinks.map((link) => (
+          <Box
+            key={link.key}
+            component="li"
+            className={
+              link.key === activeLink && !link.type ? Style.active : ''
+            }
+            sx={{ borderImageSource: info.gradient }}
+          >
+            <Link
+              to={link.to}
+              onClick={() => setActiveLink(link.key)}
+              className={Style.link}
+            >
+              {!link.type ? (
+                <p style={{ padding: '0.5rem 0' }}>{link.name}</p>
+              ) : (
+                <h1>{link.name}</h1>
+              )}
+            </Link>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
